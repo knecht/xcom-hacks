b4ec491ab48c0f825705019c830b99135348d6d9
knecht <knecht@snorc.org>
2014-09-18 00:10:53 +0200

    removed obsolete documentation

--- xcomfix.c
+++ xcomfix.c
@@ -1,21 +1,4 @@
-/*
- This is an ugly, nasty hack which should prevent the game from crashing more often than UFOs.
- Although it looks, smells and tastes pretty hacky, it worked surprisingly well for me (not a single segfault using this hack).
- Only tested with "enemy within".
-
- See also:
- http://psydk.org/archives/2014-07-05-221-introducing-plasma-fence
-
- compile:
- gcc -ldl -shared -fPIC -o libxcomfix.so -std=gnu11 xcomfix.c
-
- launch the game (after steam is started):
- 
- export LD_LIBRARY_PATH="/path/to/SteamLibrary/SteamApps/common/XCom-Enemy-Unknown/xew/binaries/linux/x86_64/"
- cd /path/to/SteamLibrary/SteamApps/common/XCom-Enemy-Unknown/xew
- LD_PRELOAD=/path/to/libxcomfix.so binaries/linux/game.x86_64
-*/
-
+/* See README.md for documentation and instructions */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
